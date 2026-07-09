6d4cf3cf71c1eeaa89b11857d1132318d4e9c41b
Subharup Biswas <biswassubharup2017@gmail.com>
2026-07-10 02:21:21 +0530

    refactor: remove unnecessary instrumentation.ts placeholder hook

--- src/instrumentation.ts
+++
@@ -1,4 +0,0 @@
-export function register() {
-  // Empty placeholder hook to force Next.js to compile an instrumentation chunk
-  // This satisfies OpenNext's boot loader on Cloudflare's edge environment
-}
